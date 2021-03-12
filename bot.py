31824e3e4f0c521dfa69b8db0d6dd07cf36719f7
whtmidoing <80530898+whtmidoing@users.noreply.github.com>
2021-03-12 18:39:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ global chatid
 global username
 global fname
 balance = 0
-bot = telebot.TeleBot("1626053042:AAEZx2S8HKPS2VYJZ0XHtCBiDkIJ4m1vbY4")
+bot = telebot.TeleBot("token")
 db = sqlite3.connect("bot.db", check_same_thread=False)
 sql = db.cursor()
 
